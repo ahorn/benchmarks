0a16fe501adf680cf4a7ff46c258b3a7653a9b5c
Lihao Liang <lihao.liang@cs.ox.ac.uk>
2013-01-11 17:05:48 +0000

    <rtc>: Add shell scripts for experiments.

---
+++ qemu-hw/rtc/run_expt.sh
@@ -0,0 +1,40 @@
+# Simple Experiment Script
+# by T.W.W. - Public Domain 2011
+
+# Parse some command line input paramters, and verify they were set
+numberOfRuns=$1
+param1=$2
+outputFolder="output-$2"
+runtimeFile="runtime-$2.txt"
+
+if [ -z $param1 ]
+then
+  echo "ERROR: Requires 2 arguments: $0 [Num Runs] [param 1]"
+  exit
+fi
+
+# Create directory for output files. Experiment will only be run if it will not overwrite an old data set
+mkdir -p $outputFolder
+
+echo "Starting experiment with $numberOfRuns iterations and param1=$param1 at " `date`
+
+# Repeat the experiment $numberOfRuns times
+for i in `seq $numberOfRuns`
+do
+  #check if you have already run this data set
+  outputFile="$outputFolder/data-$param1-$i.txt"
+  if [ -s $outputFile ]
+  then
+    echo "WARNING: File $outputFile already exists -- skipping run $i"
+    continue
+  fi
+  echo "Running iteration $i " `date`
+  
+  # Put the commands to actually run your program / system here
+  (time make verify > $outputFile) &>> $outputFolder/$runtimeFile
+  # possibly put some clean up steps here or a sleep command if you want a delay between experiments
+done
+
+# Do any final cleanup here
+
+echo "Finished at time " `date`
