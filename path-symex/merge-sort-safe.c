00a8ca4227d6d41ea5f2f6e0fc1343c45081165c
ahorn <alex.horn@cs.ox.ac.uk>
2014-04-07 21:50:23 +0100

    Add merge and quick sort as benchmarks

---
+++ path-symex/merge-sort-safe.c
@@ -0,0 +1,39 @@
+#include <assert.h>
+
+// This file contains code snippets from "Algorithms in C, Third Edition,
+// Parts 1-4," by Robert Sedgewick.
+//
+// See https://www.cs.princeton.edu/~rs/Algs3.c1-4/code.txt
+
+typedef int Item;
+#define key(A) (A)
+#define less(A, B) (key(A) < key(B))
+
+#define N 3
+
+Item aux[N];
+void merge(Item a[], int l, int m, int r)
+  { int i, j, k;
+    for (i = m+1; i > l; i--) aux[i-1] = a[i-1];
+    for (j = m; j < r; j++) aux[r+m-j] = a[j+1];
+    for (k = l; k <= r; k++)
+       if (less(aux[i], aux[j])) 
+          a[k] = aux[i++]; else a[k] = aux[j--];
+  }
+void mergesort(Item a[], int l, int r)
+  { int m = (r+l)/2;
+    if (r <= l) return;
+    mergesort(a, l, m);  
+    mergesort(a, m+1, r);
+    merge(a, l, m, r);
+  }
+
+int main() {
+  Item a[N] = {4, 2, 1};
+  mergesort(a, 0, N-1);
+
+  for (unsigned i = 0; i < N - 1; i++)
+    assert(a[i] <= a[i+1]);
+
+  return 0;
+}
---
+++ path-symex/quick-sort-unsafe.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+
+// This file contains modified code snippets from "Algorithms in C, Third Edition,
+// Parts 1-4," by Robert Sedgewick. The code is intentionally buggy!
+//
+// For the correct code, see https://www.cs.princeton.edu/~rs/Algs3.c1-4/code.txt
+
+typedef int Item;
+#define key(A) (A)
+#define less(A, B) (key(A) < key(B))
+#define exch(A, B) { Item t = A; A = B; B = t; } 
+
+int partition(Item a[], int l, int r);
+void quick_sort(Item a[], int l, int r) {
+  int i;
+  if (r < l) return;
+  //    ^ bug due to strict inequality
+  i = partition(a, l, r);
+  quick_sort(a, l, i-1);
+  quick_sort(a, i+1, r);
+}
+
+int partition(Item a[], int l, int r) {
+  int i = l-1, j = r; Item v = a[r];
+  for (;;) { 
+    while (less(a[++i], v)) ;
+    while (less(v, a[--j])) if (j == l) break;
+    if (i >= j) break;
+    exch(a[i], a[j]);
+  }
+  exch(a[i], a[r]);
+  return i;
+}
+
+int main() {
+  Item a[N];
+  quick_sort(a, 0, N-1);
+
+  for (unsigned i = 0; i < N - 1; i++)
+    assert(a[i] <= a[i+1]);
+
+  return 0;
+}
