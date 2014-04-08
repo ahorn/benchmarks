159363191ad4b066a5d6fa7f26290e3d0c207702
ahorn <alex.horn@cs.ox.ac.uk>
2014-04-08 11:17:21 +0100

    Add benchmark with recurrence sum

---
+++ path-symex/sum-recursive-unsafe.c
@@ -0,0 +1,28 @@
+// For more explanations, see Section 2.2 (Sums and Recurrences) in
+// "Concrete Mathematics", Second Edition, by Ronald L. Graham,
+// Donald E. Knuth, and Oren Patashnik
+
+#include <assert.h>
+
+int sumR(int a, int b, int k) {
+  int sum = a + b*k;
+  if (k > 0)
+    return sum + sumR(a, b, k-1);
+  else
+    return sum;
+}
+
+// N must be even
+int main() {
+  int a = nondet_int();
+  int b = nondet_int();
+
+  __CPROVER_assume(a < 16384);
+  __CPROVER_assume(b < 16384);
+
+  int result = sumR(a, b, N);
+  assert(result != ((a*(N+1)) + (b*(N+1)*(N/2))));
+  //            ^ in fact, LHS is equals equal to RHS
+
+  return 0;
+}
