e57f94ab8c70055f2561c3e42190ce4e12bba1fb
ahorn <alex.horn@cs.ox.ac.uk>
2014-04-08 18:38:02 +0100

    Use "int" instead of "unsigned int"
    This patch accommodates tools that are not bit precise.

--- path-symex/counter-unsafe.c
+++ path-symex/counter-unsafe.c
@@ -1,18 +1,20 @@
-// From SV-COMP'14 (loops/count_up_down_false.c)
+// Similar to SV-COMP'14 (loops/count_up_down_false.c) except
+// that we use explicit signedness constraints to support
+// benchmarks with tools that are not bit precise.
 #include <assert.h>
 
-unsigned int nondet_uint();
+int nondet_int();
 
 // It suffices to unwind N times
 int main() {
-  unsigned int n = nondet_uint();
-  __CPROVER_assume(n < N);
+  int n = nondet_int();
+  __CPROVER_assume(0 <= n && n < N);
 
-  unsigned int x = n, y = 0;
+  int x = n, y = 0;
   while (x > 0) {
     x = x - 1;
     y = y + 1;
   }
-  assert(y != n);
+  assert(0 <= y && y != n);
   return 0;
 }
