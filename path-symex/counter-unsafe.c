8ec611272cfd6b74d6f6152466c60b27883412e4
ahorn <alex.horn@cs.ox.ac.uk>
2014-04-08 13:10:27 +0100

    Add SV-COMP'14 benchmark that uses counters

---
+++ path-symex/counter-unsafe.c
@@ -0,0 +1,18 @@
+// From SV-COMP'14 (loops/count_up_down_false.c)
+#include <assert.h>
+
+unsigned int nondet_uint();
+
+// It suffices to unwind N times
+int main() {
+  unsigned int n = nondet_uint();
+  __CPROVER_assume(n < N);
+
+  unsigned int x = n, y = 0;
+  while (x > 0) {
+    x = x - 1;
+    y = y + 1;
+  }
+  assert(y != n);
+  return 0;
+}
