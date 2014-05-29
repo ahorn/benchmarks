4af79c856213ef284865846f3976f5e263fbbfd5
ahorn <alex.horn@cs.ox.ac.uk>
2014-05-29 19:22:39 +0100

    Describe Z3 performance quirk

--- path-symex/transform/insertion-sort-safe.cpp
+++ path-symex/transform/insertion-sort-safe.cpp
@@ -24,15 +24,15 @@ typedef int Item;
 #define key(A) (A)
 #define less(A, B) (key(A) < key(B))
 #define exch(A, B) { crv::Internal<Item> t = A; A = B; B = t; } 
-#define compexch(A, B) if (crv::sequential_dfs_checker().BRANCH_CALL(less(B, A))) exch(A, B)
+#define compexch(A, B) if (dfs_checker().BRANCH_CALL(less(B, A))) exch(A, B)
 
 void insertion_sort(crv::Internal<Item[]>& a, const crv::Internal<int>& l, const crv::Internal<int>& r) {
   crv::Internal<int> i;
-  for (i = l+1; crv::sequential_dfs_checker().BRANCH_CALL(i <= r); i = i+1) compexch(a[l], a[i]);
-  for (i = l+2; crv::sequential_dfs_checker().BRANCH_CALL(i <= r); i = i+1) {
+  for (i = l+1; dfs_checker().BRANCH_CALL(i <= r); i = i+1) compexch(a[l], a[i]);
+  for (i = l+2; dfs_checker().BRANCH_CALL(i <= r); i = i+1) {
     crv::Internal<int> j = i;
     crv::Internal<Item> v = a[i];
-    while (crv::sequential_dfs_checker().BRANCH_CALL(0 < j) && crv::sequential_dfs_checker().BRANCH_CALL(less(v, a[j-1]))) {
+    while (dfs_checker().BRANCH_CALL(0 < j) && dfs_checker().BRANCH_CALL(less(v, a[j-1]))) {
       a[j] = a[j-1]; j = j-1;
     }
     a[j] = v;
@@ -45,8 +45,8 @@ void crv_main() {
   crv::Internal<int[]> a;
 
   insertion_sort(a, 0, N-1);
-  for (crv::Internal<unsigned> i = 0; crv::sequential_dfs_checker().BRANCH_CALL(i < N - 1); i = i+1)
-    crv::sequential_dfs_checker().add_error(!(a[i] <= a[i+1]));
+  for (crv::Internal<unsigned> i = 0; dfs_checker().BRANCH_CALL(i < N - 1); i = i+1)
+    dfs_checker().add_error(!(a[i] <= a[i+1]));
 }
 
 int main() {
@@ -60,9 +60,9 @@ int main() {
       crv_main();
   
 #ifndef FORCE_BRANCH
-      error |= smt::sat == crv::sequential_dfs_checker().check();
+      error |= smt::sat == dfs_checker().check();
 #endif
-    } while (crv::sequential_dfs_checker().find_next_path() && !error);
+    } while (dfs_checker().find_next_path() && !error);
   }
 
   if (error)
